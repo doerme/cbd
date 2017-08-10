eb442e8069dd62ddb97df3b9ef5ca501bba8fb11
liangxiaoming <398921432@qq.com>
2017-08-10 13:30:44 +0800

    reg finish

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,16 @@ $('.js-loading-line').addClass('full');
 
 var app = {
     init: function(){
+        var self = this;
         $('.js-loading-wrap').addClass('hide');
         $('.js-login-wrap').removeClass('hide');
+        self.bindEven();
+    },
+    bindEven: function(){
+        $('.js-reg-bt').on('click', function(){
+            $('.js-login-wrap').addClass('hide');
+            $('.js-reg-wrap').removeClass('hide');
+        })
     }
 }
 
